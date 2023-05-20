c8a3ca9a62a5ea13f28431d59415c2ff9e251870
densyo <sinnedpenguin@proton.me>
2023-05-20 09:47:18 +0800

    Add project files.

---
+++ Penguino/Penguino.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Penguino
+{
+    partial class Penguino
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Penguino));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.addressBar = new System.Windows.Forms.TextBox();
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.goButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.v100SinnedpenguinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.refreshButton.Location = new System.Drawing.Point(46, 27);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(27, 20);
+            this.refreshButton.TabIndex = 4;
+            this.refreshButton.Text = "↻";
+            this.refreshButton.UseVisualStyleBackColor = false;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.backButton.Location = new System.Drawing.Point(12, 27);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(27, 20);
+            this.backButton.TabIndex = 3;
+            this.backButton.Text = "←";
+            this.backButton.UseVisualStyleBackColor = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.forwardButton.Location = new System.Drawing.Point(79, 27);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(27, 20);
+            this.forwardButton.TabIndex = 8;
+            this.forwardButton.Text = "→";
+            this.forwardButton.UseVisualStyleBackColor = false;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // addressBar
+            // 
+            this.addressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressBar.BackColor = System.Drawing.SystemColors.Window;
+            this.addressBar.Location = new System.Drawing.Point(112, 27);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(644, 20);
+            this.addressBar.TabIndex = 9;
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(-2, 53);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(802, 400);
+            this.chromiumWebBrowser1.TabIndex = 5;
+            this.chromiumWebBrowser1.LoadingStateChanged += new System.EventHandler<CefSharp.LoadingStateChangedEventArgs>(this.chromiumWebBrowser1_LoadingStateChanged);
+            // 
+            // goButton
+            // 
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.goButton.Location = new System.Drawing.Point(761, 27);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(27, 20);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "➤";
+            this.goButton.UseVisualStyleBackColor = false;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(27, 20);
+            this.menuToolStripMenuItem.Text = "≡";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.v100SinnedpenguinToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(28, 20);
+            this.toolStripMenuItem1.Text = "❓";
+            // 
+            // v100SinnedpenguinToolStripMenuItem
+            // 
+            this.v100SinnedpenguinToolStripMenuItem.Name = "v100SinnedpenguinToolStripMenuItem";
+            this.v100SinnedpenguinToolStripMenuItem.Size = new System.Drawing.Size(200, 22);
+            this.v100SinnedpenguinToolStripMenuItem.Text = "v1.0.0 © sinnedpenguin";
+            this.v100SinnedpenguinToolStripMenuItem.Click += new System.EventHandler(this.v100SinnedpenguinToolStripMenuItem_Click);
+            // 
+            // Penguino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.addressBar);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.backButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Penguino";
+            this.Text = "Penguino";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.TextBox addressBar;
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem v100SinnedpenguinToolStripMenuItem;
+    }
+}
+
---
+++ Penguino/Penguino.cs
@@ -0,0 +1,94 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Penguino
+{
+    public partial class Penguino : Form
+    {
+        public Penguino()
+        {
+            InitializeComponent();
+            addressBar.Select();
+            addressBar.KeyPress += addressBar_KeyPress;
+        }
+
+        // Load the specified URL in the browser
+        public void Browse()
+        {
+            chromiumWebBrowser1.Load(addressBar.Text);
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            Browse();
+        }
+
+        // Allow user to go visit a webpage by pressing the Enter key
+        private void addressBar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the Enter key is pressed
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true; // Prevent the Enter key from producing a beep sound
+                Browse(); // Call to load the specified URL
+            }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            chromiumWebBrowser1.Back();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            chromiumWebBrowser1.Reload();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            chromiumWebBrowser1.Forward();
+        }
+
+        private void chromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            // Update the address bar text when the browser is loading a new page
+            if (e.IsLoading)
+            {
+                Invoke((MethodInvoker)(() =>
+                {
+                    addressBar.Text = e.Browser.MainFrame.Url;
+                }));
+            }
+            // Update the address bar text when the browser finishes loading a page
+            Invoke((MethodInvoker)(() =>
+            {
+                addressBar.Text = e.Browser.MainFrame.Url;
+            }));
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit the application when the "Exit" menu option is clicked
+            Application.Exit();
+        }
+
+        private void v100SinnedpenguinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 
+            string url = "https://github.com/sinnedpenguin";
+            addressBar.Text = url;
+            Browse();
+            //
+        }
+    }
+}
